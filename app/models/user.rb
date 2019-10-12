5873473446ec0f902108eee8a6612a8e202c297d
tokuchi0823 <tokuchi.ryu.th@gmail.com>
2019-10-12 07:53:23 +0000

    Finish full-fledged-login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   before_save { self.email = email.downcase }
   
   validates :name,  presence: true, length: { maximum: 50 }
@@ -21,6 +22,11 @@ class User < ApplicationRecord
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
+  end
+  
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
   
   def forget
