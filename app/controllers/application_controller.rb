cfa53a70a10b933e7f4b5afd2d5427d2cd8897e2
tokuchi0823 <tokuchi.ryu.th@gmail.com>
2019-10-24 10:16:01 +0000

    test2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -44,7 +44,7 @@ class ApplicationController < ActionController::Base
     @attendances = @user.attendances.where(worked_on: @first_day..@last_day).order(:worked_on)
   end
 
-  rescue ActiveRecord::RecordInvalid
+ rescue ActiveRecord::RecordInvalid
    flash[:danger] = "ページ情報の取得に失敗しました、再アクセスしてください。"
    redirect_to root_url
  end
